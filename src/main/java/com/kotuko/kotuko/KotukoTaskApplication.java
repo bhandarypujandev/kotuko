2118a15b2e2a00b7b6d65fd0236ef3be5d532a4e
bhandarypujandev <pujan.bhandari@digihub.com.np>
2023-08-30 21:31:55 +0545

    Initial commit

---
+++ src/main/java/com/kotuko/kotuko/KotukoTaskApplication.java
@@ -0,0 +1,13 @@
+package com.kotuko.kotuko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotukoTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KotukoTaskApplication.class, args);
+    }
+
+}
